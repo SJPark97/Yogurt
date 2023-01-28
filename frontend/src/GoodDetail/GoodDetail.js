0ffd5d3f52d74c2a9e60e7d6dad627b9c9808af0
송은지 <cjsdks518@naver.com>
2023-01-28 18:29:42 +0900

    Fix-detail css수정

--- frontend/src/GoodDetail/GoodDetail.js
+++ frontend/src/GoodDetail/GoodDetail.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import dummy from '../db/list.json';
 import BackToTop from '../AppBar/BackToTop';
+import Carousel from '../Common/Carousel';
+
 import './GoodDetail.css';
 
 function GoodDetail() {
@@ -14,7 +16,7 @@ function GoodDetail() {
     <div>
       <BackToTop />
       <img src={post.image} alt="이미지사진" />
-      <div className="basic-info">
+      <div className="detail-basic-info">
         <div className="detail-post-store">{post.post_store}</div>
         <div className="detail-post-title">{post.post_title}</div>
         <div className="detail-br-cateId">{post.br_cateId}</div>
@@ -31,6 +33,24 @@ function GoodDetail() {
         </div>
       </div>
       <hr />
+      <div className="detail-size-info">
+        <div className="detail-size-name">사이즈</div>
+        <div className="detail-size">{post.post_size}</div>
+      </div>
+      <hr />
+      <div className="detail-detail-info">
+        <div className="detail-detail-name">상세설명</div>
+        {/* 왜 왼쪽정렬안되지? */}
+        <div className="detail-detail">{post.post_content}</div>
+      </div>
+      <hr />
+      <div className="detail-store-info">
+        <div className="detail-store-name">{post.post_store}님의 다른 상품</div>
+        <div className="detail-store-post">
+          {/* 상점명 상품번호 넘겨서 할건가? 어떻게 할지 생각해보기 */}
+          <Carousel card={dummy.Popular} />
+        </div>
+      </div>
     </div>
   );
 }
