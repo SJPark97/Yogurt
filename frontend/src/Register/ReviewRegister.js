b20f66f9e3c1bf27db37be625f289aa34977b141
송은지 <cjsdks518@naver.com>
2023-01-30 14:26:54 +0900

    FEAT-review등록페이지

---
+++ frontend/src/Register/ReviewRegister.js
@@ -0,0 +1,56 @@
+import BackToTop from '../AppBar/BackToTop';
+import './ReviewRegister.css';
+
+function ReviewRegister() {
+  return (
+    <div className="reviewregister">
+      <BackToTop />
+      <form action="/review/join" method="post">
+        <div className="review_reg_title">
+          <p>리뷰 제목</p>
+          <input
+            type="text"
+            id="review_reg_title"
+            name="title"
+            placeholder="리뷰 제목을 입력해주세요"
+          />
+        </div>
+        <hr />
+        <div className="review_reg_star">
+          <p> 별점</p>
+          <div className="review_star-ratings">
+            <div className="review_star-ratings-fill">
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+            </div>
+            <div className="review_star-ratings-base">
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+              <span>★</span>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+        <div className="review_reg_detail">
+          <p>리뷰 내용</p>
+          <textarea
+            id="review_reg_detail"
+            name="size"
+            placeholder="리뷰 내용을 적어주세요"
+          />
+        </div>
+        <div id="submit_btn">
+          <button type="submit">저장</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ReviewRegister;
