d5041b6cb1b7d29917797a531fd792d7c0141b0c
SJPark97 <tjwlsdud33@naver.com>
2023-01-30 10:11:42 +0900

    FEAT F : 알림 아이템 및 페이지 제작, SJ.json 데이터 파일 생성

--- frontend/src/Common/CardList.js
+++ frontend/src/Common/CardList.js
@@ -4,18 +4,17 @@ import './CardList.css';
 
 function CardList(props) {
   const { data } = props;
+  const discount = Math.floor(
+    ((data.post_price - data.post_sale_price) / data.post_price) * 100,
+  );
+
   return (
     <div className="Card">
       <Link to={`/post/${data.postId}`}>
         <img className="CardImg" src={data.postimage_url} alt="#" />
         <div className="CardBrand">{data.br_cateName}</div>
         <div className="CardName">{data.post_title}</div>
-        <div className="CardDiscount">
-          {Math.floor(
-            ((data.post_price - data.post_sale_price) / data.post_price) * 100,
-          )}
-          %
-        </div>
+        {discount ? <div className="CardDiscount">{discount}%</div> : null}
         <div className="CardPrice">
           {data.post_sale_price.toLocaleString()}원
         </div>
