61773bbe6f0035ec843e69447da72437374fae4d
caerinso <so992419@naver.com>
2023-02-03 20:55:54 +0900

    "Feat -B 카카오페이 - 카카오페이 구현 "

--- backend/src/main/java/com/ssafy/common/api/kakaopay/KakaoPayReadyVO.java
+++ backend/src/main/java/com/ssafy/common/api/kakaopay/KakaoPayReadyVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-public class KakaoPayReadyV0 {
+public class KakaoPayReadyVO {
     //response
     private String tid, next_redirect_pc_url;
     private Date created_at;
