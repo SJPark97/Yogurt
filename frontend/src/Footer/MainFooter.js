35e2fb236c6daa05ba74b98af2b517b77c225b73
BaeSeongHyeon <tjdgus6937@naver.com>
2023-02-03 11:06:50 +0900

    FIX-카테고리 페이지 css 수정 및 페이지별 Footer 설정

--- frontend/src/Footer/MainFooter.js
+++ frontend/src/Footer/MainFooter.js
@@ -1,107 +1,23 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import HomeIcon from '@mui/icons-material/Home';
-import StoreIcon from '@mui/icons-material/Store';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Paper from '@mui/material/Paper';
-import { useEffect } from 'react';
+import { useLocation, useMatch } from 'react-router-dom';
+import Footer from './Footer';
 
-import { styled } from '@mui/material/styles';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-
-const CustomBottomNavigationAction = styled(BottomNavigationAction)`
-  color: #bdbdbd;
-  max-width: 100%;
-
-  .MuiBottomNavigationAction-label {
-    background: #ffffff;
-  }
-
-  &.Mui-selected {
-    color: #ff3d00;
-  }
-`;
-
-export default function MainFooter({ user }) {
-  const [value, setValue] = React.useState();
-  const ref = React.useRef(null);
-  const navigate = useNavigate();
+function MainAppBar({ user }) {
   const pageUrl = useLocation().pathname;
-
-  useEffect(() => {
-    if (pageUrl === '/') {
-      setValue(0);
-    } else if (pageUrl === '/stores') {
-      setValue(1);
-    } else if (pageUrl === '/alarms') {
-      setValue(2);
-    } else if (pageUrl.includes('/profile')) {
-      setValue(3);
-    }
-  }, [pageUrl]);
-
-  // 유저정보
-  let userRole;
-  if (user.role === 'buyer') {
-    userRole = 'buyer';
-  } else {
-    userRole = 'seller';
-  }
+  // const [appBar, setAppBar] = useState('/');
+  // useEffect(() => {
+  //   setAppBar(pageUrl);
+  // }, [pageUrl]);
+  console.log('이거다!', pageUrl);
 
   return (
-    <Box sx={{ pb: 7 }} ref={ref}>
-      <CssBaseline />
-      <Paper
-        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        >
-          <CustomBottomNavigationAction
-            component={Link}
-            to="/"
-            label="메인"
-            icon={<HomeIcon />}
-            // onClick={() => navigate("/")} 바로 이동할때는 Link쓰기
-          />
-          <CustomBottomNavigationAction
-            component={Link}
-            to="/stores"
-            label="상점"
-            icon={<StoreIcon />}
-          />
-          <CustomBottomNavigationAction
-            component={Link}
-            to="/alarms"
-            label="알림"
-            icon={<NotificationsIcon />}
-          />
-          <CustomBottomNavigationAction
-            label="프로필"
-            icon={<AccountCircleIcon />}
-            onClick={() =>
-              userRole === 'buyer'
-                ? navigate('/profile/buyer')
-                : navigate('/profile/seller')
-            } // 이거는 로그인 안됐을 때 로그인 페이지로 가게
-            // sx={[
-            //   {
-            //     color: "#bdbdbd",
-            //     "&.Mui-selected": { color: "#ff3d00", background: "#ffffff" },
-            //   },
-            // ]}
-          />
-        </BottomNavigation>
-      </Paper>
-    </Box>
+    <div>
+      {useMatch('/') && <Footer user={user} />}
+      {useMatch('/stores') && <Footer user={user} />}
+      {useMatch('/alarms') && <Footer user={user} />}
+      {useMatch('/profile/buyer') && <Footer user={user} />}
+      {useMatch('/profile/seller') && <Footer user={user} />}
+    </div>
   );
 }
+
+export default MainAppBar;
