c8df642e694747230ea9da1fafb4b5eb85c9224d
BaeSeongHyeon <tjdgus6937@naver.com>
2023-02-06 17:32:45 +0900

    다시올려유~

--- frontend/src/Category/Category.js
+++ frontend/src/Category/Category.js
@@ -1,14 +1,12 @@
 import './Category.css';
 
 function Category({ sub }) {
+  const handleClick = () => {
+    console.log('서브카테고리', sub);
+  };
+
   return (
-    <div
-      role="presentation"
-      className="category"
-      onClick={() => {
-        console.log('서브카테고리', sub.name);
-      }}
-    >
+    <div role="presentation" className="category" onClick={handleClick}>
       <img src={sub.image} alt="#" className="categoryImg" />
       <div className="subName">{sub.name}</div>
     </div>
