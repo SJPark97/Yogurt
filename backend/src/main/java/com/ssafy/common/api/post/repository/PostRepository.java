7a9bca9756c51da20bf6326fc8fe741ab09be34e
caerinso <so992419@naver.com>
2023-02-06 16:42:45 +0900

    "Feat -B search 검색 기능 - post 검색 구현 - 브랜드 검색 구현 - 판매자 검색 구현 "

--- backend/src/main/java/com/ssafy/common/api/post/repository/PostRepository.java
+++ backend/src/main/java/com/ssafy/common/api/post/repository/PostRepository.java
@@ -5,7 +5,10 @@ import com.ssafy.common.api.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @Transactional
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findAllByTitleContaining(String searchKeyword);
 }
