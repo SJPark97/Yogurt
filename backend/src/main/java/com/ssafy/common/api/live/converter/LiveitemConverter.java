c82fe6fe7e993ead2654d7f5842de070fba02594
왕현석 <sjj02055@gmail.com>
2023-02-07 16:34:08 +0900

    Feat B : 라이브 물품(Livelist) 조회 추가

--- backend/src/main/java/com/ssafy/common/api/live/converter/LiveitemConverter.java
+++ backend/src/main/java/com/ssafy/common/api/live/converter/LiveitemConverter.java
@@ -3,6 +3,7 @@ package com.ssafy.common.api.live.converter;
 
 import com.ssafy.common.api.live.domain.LiveList;
 import com.ssafy.common.api.live.domain.LiveRoom;
+import com.ssafy.common.api.live.domain.LivelistStatus;
 import com.ssafy.common.api.post.domain.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,8 +16,7 @@ public class LiveitemConverter {
         return LiveList.builder()
                 .post(post)
                 .liveRoom(liveRoom)
+                .status(LivelistStatus.STATUS_ACTIVE)
                 .build();
     }
-
-
 }
---
+++ backend/src/main/java/com/ssafy/common/api/live/domain/LivelistStatus.java
@@ -0,0 +1,6 @@
+package com.ssafy.common.api.live.domain;
+
+public enum LivelistStatus {
+    STATUS_DELETE,
+    STATUS_ACTIVE
+}
