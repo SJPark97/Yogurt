78ccbcdc455c4e6d74d41927635cdc8018ce1d41
hyunho <potato3884@naver.com>
2023-02-07 16:33:06 +0900

    Feat B : 라이브 물품(Livelist)  조회 추가  라이브  등록 , 라이브 삭제 시에는 livelist들을 함께 설정하는 부분 추가

--- backend/src/main/java/com/ssafy/common/api/live/domain/LiveRoom.java
+++ backend/src/main/java/com/ssafy/common/api/live/domain/LiveRoom.java
@@ -2,6 +2,8 @@ package com.ssafy.common.api.live.domain;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.ssafy.common.api.user.domain.User;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 import javax.persistence.*;
@@ -22,36 +24,50 @@ public class LiveRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "liveroom_id")
+    @ApiModelProperty(example="1")
+    @ApiParam(value = "id")
     private Long id;
 
     // 판매자 고유 번호 sellerId
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonManagedReference
     @JoinColumn(name = "seller_id")
+    @ApiModelProperty(example="User 객체 : {}")
+    @ApiParam(value = "User")
     private User seller;
 
 
     //생성시간 liveroom_created
     @NonNull
     @Column(name = "liveroom_created")
+    @ApiModelProperty(example="2023-02-01T11:22:11.000")
+    @ApiParam(value = "created")
     private Timestamp created ;
 
     //  라이브 룸 제목
     @NonNull
     @Column(name = "liveroom_title")
+    @ApiModelProperty(example="올해 가을 신상!!!")
+    @ApiParam(value = "title")
     private String title;
 
     // 라이브 대표이미지
     @Column(name = "liveroom_thumbnail")
+    @ApiModelProperty(example="wnwaksd.wakasnkldsa/wkldalnw/awkd.jpg")
+    @ApiParam(value = "thumbnail")
     private String thumbnail ;
 
     //라이브 룸 상태
     @NonNull
     @Column(name = "liveroom_status")
+    @ApiModelProperty(example="STATUS_ONAIR")
+    @ApiParam(value = "thumbnail")
     private LiveRoomStatus status;
 
     @NonNull
     @Column(name = "liveroom_time")
+    @ApiModelProperty(example="2023-02-01T11:22:11.000")
+    @ApiParam(value = "time")
     private Timestamp time ;
 
     public void update_status(LiveRoomStatus status){
