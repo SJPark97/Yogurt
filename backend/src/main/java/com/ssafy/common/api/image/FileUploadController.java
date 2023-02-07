a105aecc8dee639a88e361ba2282d1cf8a688eeb
왕현석 <sjj02055@gmail.com>
2023-02-07 16:27:36 +0900

    Api operation 작성

--- backend/src/main/java/com/ssafy/common/api/image/FileUploadController.java
+++ backend/src/main/java/com/ssafy/common/api/image/FileUploadController.java
@@ -1,7 +1,6 @@
 package com.ssafy.common.api.image;
 
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
 @RequiredArgsConstructor
 @RestController
 public class FileUploadController {
