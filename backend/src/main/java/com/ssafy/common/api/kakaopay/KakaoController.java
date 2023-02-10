e4b48b1ef1ab501b85731f81e796e4e90d2dfa20
송은지 <cjsdks518@naver.com>
2023-02-10 13:58:36 +0900

    "Fix -B post/like 수정

--- backend/src/main/java/com/ssafy/common/api/kakaopay/KakaoController.java
+++ backend/src/main/java/com/ssafy/common/api/kakaopay/KakaoController.java
@@ -3,6 +3,8 @@ package com.ssafy.common.api.kakaopay;
 import lombok.Setter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,10 +28,11 @@ public class KakaoController {
         return "redirect:" + kakaopay.kakaoPayReady();
     }
     @GetMapping("/kakaoPaySuccess")
-    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+    public ResponseEntity<KakaoPayApprovalVO> kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
         log.info("kakaoPaySuccess get...............................");
         log.info("kakaoPaySuccess pg_token : " + pg_token);
         model.addAttribute("info", kakaopay.kakaoPayInfo(pg_token));
         System.out.println(model.addAttribute("info", kakaopay.kakaoPayInfo(pg_token)));
+        return new ResponseEntity<>(kakaopay.kakaoPayInfo(pg_token), HttpStatus.OK);
     }
 }
