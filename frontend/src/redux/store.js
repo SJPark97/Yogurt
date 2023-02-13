e8bfc9cc615e75f01869a9d8d59768645c8cc90a
BaeSeongHyeon <tjdgus6937@naver.com>
2023-02-13 11:29:21 +0900

    FEAT-redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
