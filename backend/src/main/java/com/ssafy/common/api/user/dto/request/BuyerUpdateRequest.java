80e3b8c4b70be25b0c7f971cd3dcbc3d19f6b36b
왕현석 <sjj02055@gmail.com>
2023-02-14 11:27:33 +0900

    "Feat B 구매자 판매자 정보수정"

---
+++ backend/src/main/java/com/ssafy/common/api/user/dto/request/BuyerUpdateRequest.java
@@ -0,0 +1,9 @@
+package com.ssafy.common.api.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class BuyerUpdateRequest {
+    private String nickName;
+    private String profileImage;
+}
