484c67a8f64b9bc8c57443063ae76e33465db01d
SJPark97 <tjwlsdud33@naver.com>
2023-02-15 13:43:32 +0900

    TEST F : 카카오페이 결제 완료 페이지 테스트

--- frontend/src/Payment/Test.js
+++ frontend/src/Payment/Test.js
@@ -1,20 +1,23 @@
 import {React, useCallback, useEffect} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Test() {
 	const token = window.location.href.replace("https://i8b204.p.ssafy.io/kakaopay/success?pg_token=", "")
 	const navigate = useNavigate();
+	const loginUser = useSelector(state => state.user.value);
 
 	const success = useCallback(async () => {
     await axios
       .get('https://i8b204.p.ssafy.io/be-api/kakaoPaySuccess', {
         params: { pg_token: token },
+				headers: {Authorization: loginUser.token}
       })
       .then(res => {
         setTimeout(navigate('/'), 2000);
       });
-  }, [token, navigate]);
+  }, [token, navigate, loginUser]);
 	
 
   useEffect(() => {
