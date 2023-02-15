194148111db9c7ee1a8d3095289a152893ac8bf0
SJPark97 <tjwlsdud33@naver.com>
2023-02-15 13:38:20 +0900

    TEST F : 카카오페이 결제 완료 페이지 테스트

--- frontend/src/Payment/Test.js
+++ frontend/src/Payment/Test.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import {React, useCallback, useEffect} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Test() {
+	const token = window.location.href.replace("https://i8b204.p.ssafy.io/kakaopay/success?pg_token=", "")
+	const navigate = useNavigate();
+
+	const success = useCallback(async () => {
+    await axios
+      .get('https://i8b204.p.ssafy.io/be-api/kakaoPaySuccess', {
+        params: { pg_token: token },
+      })
+      .then(res => {
+        setTimeout(navigate('/'), 2000);
+      });
+  }, [token, navigate]);
+	
+
+  useEffect(() => {
+    success();
+  }, [success]);
+
   return <div>
-		<h1>{window.location.href.replace("https://i8b204.p.ssafy.io/kakaopay/success?pg_token=", "")}</h1>
+		<h1>결제 성공</h1>
+		<h2>2초 뒤에 홈화면으로 이동합니다.</h2>
 	</div>;
 }
