852f12eb7ff6ff535a8bcc635cf4008f2e42f9e1
BaeSeongHyeon <tjdgus6937@naver.com>
2023-02-15 14:03:17 +0900

    FIX-메인페이지 캐러셀 수정

--- frontend/src/PostDetail/DetailCarousel.js
+++ frontend/src/PostDetail/DetailCarousel.js
@@ -9,26 +9,26 @@ import './DetailCarousel.css';
 // import required modules
 import { Pagination } from 'swiper';
 
-export default function DetailCarousel({images}) {
-
-    return (
-        <Swiper
-            style={{
-                '--swiper-navigation-color': '#fff',
-                '--swiper-pagination-color': '#fff',
-            }}
-            pagination={true}
-            slidesPerView={1}
-            spaceBetween={30}
-            loop={true}
-            modules={[Pagination]}
-            className="mySwiper"
-        >
-            {images && images.map(image => (
-                <SwiperSlide>
-                    <img className="detail_img" src={image.url} alt="이미지사진" />
-                </SwiperSlide>
-            ))}
-        </Swiper>
-    );
+export default function DetailCarousel({ images }) {
+  return (
+    <Swiper
+      style={{
+        '--swiper-navigation-color': '#deb887',
+        '--swiper-pagination-color': '#deb887',
+      }}
+      pagination={true}
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      {images &&
+        images.map(image => (
+          <SwiperSlide key={image.url}>
+            <img className="detail_img" src={image.url} alt="이미지사진" />
+          </SwiperSlide>
+        ))}
+    </Swiper>
+  );
 }
