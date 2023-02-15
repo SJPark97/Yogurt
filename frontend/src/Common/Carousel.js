b35364be65566b244888a70646d21611b488331f
caerinso <so992419@naver.com>
2023-02-15 17:43:28 +0900

    Add-라이브 상품 선택 가능

--- frontend/src/Common/Carousel.js
+++ frontend/src/Common/Carousel.js
@@ -7,7 +7,9 @@ function Carousel({ list }) {
     <div>
       <div className="Carousel">
         {list.map(data => (
+          <div style={{marginRight: '4vw'}}>
           <CardList data={data} key={data.id} />
+          </div>
         ))}
       </div>
     </div>
